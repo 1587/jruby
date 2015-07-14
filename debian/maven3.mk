0d11771408d2d6b48b5117661d68ac6f9dd6f68b
Miguel Landaeta <nomadium@debian.org>
2015-07-14 20:17:21 -0300

    Fix FTBFS due to Maven 3.3.x changes

--- debian/maven3.mk
+++ debian/maven3.mk
@@ -6,6 +6,8 @@ MAVEN_HOME = /usr/share/maven
 DEB_CLASSPATH = $(MAVEN_HOME)/boot/plexus-classworlds-2.x.jar
 DEB_MAVEN_INVOKE = cd $(DEB_BUILDDIR) && $(JAVACMD) -classpath $(DEB_CLASSPATH) \
                  $(JAVA_OPTS) -Dclassworlds.conf=$(CURDIR)/debian/m2.conf \
+                 -Dmaven.home=/usr/share/maven \
+                 -Dmaven.multiModuleProjectDirectory=$(CURDIR) \
                  org.codehaus.plexus.classworlds.launcher.Launcher \
                  -s/etc/maven2/settings-debian.xml \
                  -Dmaven.repo.local=$(DEB_MAVEN_REPO) \
